19364277d6c492940389e7faa4a6d14a91887f84
Administrator <513213884@qq.com>
2016-12-20 17:41:18 +0800

    copy

---
+++ src/main/java/com/tian/Medicine.java
@@ -0,0 +1,48 @@
+package com.tian;
+
+/**
+ * Created by Administrator on 2016/12/20.
+ */
+public class Medicine {
+    private Integer id;
+    private String name;
+    private String function;
+
+    public Medicine() {
+        super();
+    }
+
+    public Medicine(Integer id, String name, String function) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.function = function;
+    }
+
+    //getter and  setter ()
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFunction() {
+        return function;
+    }
+
+    public void setFunction(String function) {
+        this.function = function;
+    }
+}
